c387179b9d2699eb11a74da166f0855bdefbc80e
Akash G <ganeshakas2004@gmail.com>
2024-01-06 00:34:08 +0530

    major changes

--- App.js
+++ App.js
@@ -1,46 +1,28 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+import LoginScreen from './screens/LoginScreen';
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+const Stack = createNativeStackNavigator();
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
 
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
+
+const App = () => {
+  const[fontloaded]=useFonts({
+    'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'poppins-light': require('./assets/fonts/Poppins-Light.ttf'),
+    'poppins-medium': require('./assets/fonts/Poppins-Medium.ttf'),
+    'poppins-semibold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+  })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" options={{ headerShown: false }} component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
---
+++ screens/LoginScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, KeyboardAvoidingView, TouchableOpacity, TextInput, ActivityIndicator, TouchableWithoutFeedback, Image, Keyboard } from 'react-native'
+import Animated, { FadeInUp, FadeInDown,  } from 'react-native-reanimated';
+
+const LoginScreen = () => {
+    return (
+        <TouchableWithoutFeedback
+            onPress={() => Keyboard.dismiss()}
+        >
+            <KeyboardAvoidingView
+                className="flex-1 bg-white"
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            >
+                <View className="flex-1 mb-[12%]  flex-row justify-center items-end">
+                    <Animated.Image
+                        entering={FadeInUp.delay(200).duration(1000).springify()}
+                        source={require('../assets/images/logo.jpg')}
+                        style={{ width: 250, height: 180 }}
+                    />
+                </View>
+
+                <View
+                    className="flex-1  px-4 pt-10 space-y-3 border border-[#dadde0]"
+                    style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
+                >
+                    <View className="flex items-center mx-5 space-y-4">
+
+                        <Animated.View
+                            entering={FadeInDown.duration(1000).springify()}
+                            className="border p-[5%] rounded-2xl w-full">
+                            <TextInput
+                                placeholder="Email Address"
+                                placeholderTextColor={'gray'}
+                            />
+                        </Animated.View>
+
+                        <Animated.View
+                            entering={FadeInDown.delay(200).duration(1000).springify()}
+                            className=" border p-[5%] rounded-2xl w-full mb-3">
+                            <TextInput
+                                placeholder="Password"
+                                placeholderTextColor={'gray'}
+                                secureTextEntry
+                            />
+                        </Animated.View>
+
+                        <Animated.View
+                            className="w-full"
+                            entering={FadeInDown.delay(400).duration(1000).springify()}>
+
+                            <TouchableOpacity className="w-full bg-emerald-800 p-[4%] rounded-xl mb-3">
+                                <Text className="text-base text-white  text-center">Sign In</Text>
+                            </TouchableOpacity>
+                        </Animated.View>
+                    </View>
+                </View>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+    )
+}
+
+export default LoginScreen
