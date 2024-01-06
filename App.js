cfdeaf7216d061487532e484f0aad7741f7cd16a
Akash G <ganeshakas2004@gmail.com>
2024-01-06 13:38:28 +0530

    fonts implemented

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
 import LoginScreen from './screens/LoginScreen';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const Stack = createNativeStackNavigator();
 
-
-
 const App = () => {
-  const[fontloaded]=useFonts({
+
+ const [fontsLoaded] = useFonts({
     'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
     'poppins-light': require('./assets/fonts/Poppins-Light.ttf'),
     'poppins-medium': require('./assets/fonts/Poppins-Medium.ttf'),
     'poppins-semibold': require('./assets/fonts/Poppins-SemiBold.ttf'),
-  })
+  });
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  const loadAssetsAsync = async () => {
+    await SplashScreen.hideAsync();
+  };
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      loadAssetsAsync().then(() => setAppIsReady(true));
+    }
+  }, [fontsLoaded]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
     <NavigationContainer>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, KeyboardAvoidingView, TouchableOpacity, TextInput, ActivityIndicator, TouchableWithoutFeedback, Image, Keyboard } from 'react-native'
-import Animated, { FadeInUp, FadeInDown,  } from 'react-native-reanimated';
+import Animated, { FadeInUp, FadeInDown, } from 'react-native-reanimated';
 
 const LoginScreen = () => {
     return (
@@ -14,33 +14,34 @@ const LoginScreen = () => {
                 <View className="flex-1 mb-[12%]  flex-row justify-center items-end">
                     <Animated.Image
                         entering={FadeInUp.delay(200).duration(1000).springify()}
-                        source={require('../assets/images/logo.jpg')}
+                        source={require('../assets/images/logo.jpeg')}
                         style={{ width: 250, height: 180 }}
                     />
                 </View>
 
                 <View
-                    className="flex-1  px-4 pt-10 space-y-3 border border-[#dadde0]"
+                    className="flex-1   px-4 pt-10 space-y-3 border border-[#dadde0]"
                     style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
                 >
                     <View className="flex items-center mx-5 space-y-4">
-
                         <Animated.View
                             entering={FadeInDown.duration(1000).springify()}
-                            className="border p-[5%] rounded-2xl w-full">
+                            className="border border-gray-500 p-[5%] rounded-2xl w-full">
                             <TextInput
                                 placeholder="Email Address"
                                 placeholderTextColor={'gray'}
+                                style={{ fontFamily: 'poppins-semibold' }}
                             />
                         </Animated.View>
 
                         <Animated.View
                             entering={FadeInDown.delay(200).duration(1000).springify()}
-                            className=" border p-[5%] rounded-2xl w-full mb-3">
+                            className=" border p-[5%] border-gray-500 rounded-2xl w-full mb-3">
                             <TextInput
                                 placeholder="Password"
                                 placeholderTextColor={'gray'}
                                 secureTextEntry
+                                style={{ fontFamily: 'poppins-semibold' }}
                             />
                         </Animated.View>
 
@@ -49,7 +50,7 @@ const LoginScreen = () => {
                             entering={FadeInDown.delay(400).duration(1000).springify()}>
 
                             <TouchableOpacity className="w-full bg-emerald-800 p-[4%] rounded-xl mb-3">
-                                <Text className="text-base text-white  text-center">Sign In</Text>
+                                <Text className="font-medium text-white  text-center" style={{ fontFamily: 'poppins-semibold' }}>Sign In</Text>
                             </TouchableOpacity>
                         </Animated.View>
                     </View>
